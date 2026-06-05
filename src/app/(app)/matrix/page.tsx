ccc11f2d762152a95f10d961915bca39eaf85ccd
Sumalya Dey <sumalya_us2603cdh66@iitp.ac.in>
2026-06-05 13:44:22 +0530

    fix: disable caching on matrix page

--- src/app/(app)/matrix/page.tsx
+++ src/app/(app)/matrix/page.tsx
@@ -1,6 +1,9 @@
 import { createClient } from "@/utils/supabase/server";
 import { MatrixDashboard } from "./MatrixDashboard";
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export default async function MatrixPage() {
   const supabase = await createClient();
   const { data: { user } } = await supabase.auth.getUser();
